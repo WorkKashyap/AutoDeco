b95784a73568810ec3a8c520d9836add0d19b237
urjagandhi-it <urjagandhi4@gmail.com>
2021-03-17 15:49:11 +0530

    navbar chngs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.103:1122/api'
-  //apiUrl: 'http://localhost:1122/api'
+  //apiUrl: 'http://192.168.0.103:1122/api'
+  apiUrl: 'http://localhost:1122/api'
   // apiUrl: 'http://10.25.2.204:1122/api'
  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://192.168.0.103:1122/api",
-  //apiUrl: "http://localhost:1122/api",
+  //apiUrl: "http://192.168.0.103:1122/api",
+  apiUrl: "http://localhost:1122/api",
 
   // apiUrl: "http://103.236.154.122:1111/api"
   //  apiUrl: "http://103.236.154.122:1111/api"
